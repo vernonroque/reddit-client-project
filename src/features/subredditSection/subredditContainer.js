8d84842902c16612dec551072e38b37933db7f34
Vernon Fernandez Roque <vroque88@gmail.com>
2021-08-26 20:08:56 -0500

    Add documents for features and laying out the application

---
+++ src/features/subredditSection/subredditContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export const SubredditContainer = () => {
+
+    return(
+        <>
+        <section className="subreddit_container">
+            <h1>HELLO HELLO</h1>
+            <h3 className="subreddits_title">Subreddits</h3>
+        </section>
+        </>
+    );
+} 
